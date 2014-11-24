a0a8b617abf0c60a124195db363369ef3331f194
Ivan D Vasin <nisavid@gmail.com>
2014-11-24 15:49:23 -0500

    bump version string

--- spruce/iri/__init__.py
+++ spruce/iri/__init__.py
@@ -7,7 +7,7 @@ This is an extension of :mod:`urllib` and :mod:`urlparse`.
 
 """
 
-__version__ = "0.1.2"
+__version__ = "0.2.0"
 __copyright__ = "Copyright (C) 2014 Ivan D Vasin"
 __credits__ = ["Ivan D Vasin"]
 __maintainer__ = "Ivan D Vasin"
